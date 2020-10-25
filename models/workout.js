df6b718816e3f4a2cc26143d814757fa09c2c0ad
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-10-25 00:03:07 -0400

    server routes and model

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        required: "Date is required",
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+				type: String,
+				trim: true,
+				required: 'Please add an exercise type.'
+			},
+			name: {
+				type: String,
+				trim: true,
+				required: 'Please provide an exercise name',
+			},
+			distance: {
+				type: Number,
+				trim: true
+			},
+			duration: {
+				type: Number,
+				trim: true
+			},
+			weight: {
+				type: Number,
+				trim: true
+			},
+			sets: {
+				type: Number,
+				trim: true
+			},
+			reps: {
+				type: Number,
+				trim: true
+			} 
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+require('dotenv').config()
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(logger('dev'));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// Set up api routes
+app.use('/api', apiRoutes);
+
+// Set up html routes
+app.use('/', htmlRoutes);
+
+// connect to Mongo database with Mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+}
+);
+
+// Listener
+app.listen(PORT, () => {
+	console.log(`App running on port ${PORT}!`);
+});
